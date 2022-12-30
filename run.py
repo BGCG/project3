5efbb6595274cb3902aa7778c57dcfb855c9c455
BGCG <billie.griffith4@gmail.com>
2022-12-30 10:21:01 +0000

    Create basic structure of validation of user input functions for ABO and Rh inputs

--- run.py
+++ run.py
@@ -16,3 +16,45 @@ donations = SHEET.worksheet('donations')
 
 donation_data = donations.get_all_values()
 print(donation_data)
+
+
+def validate_abo_data():
+    """
+    Validate ABO input data and add to sheet
+    """
+    abo_inputted = False
+    while abo_inputted is False:
+        try:
+            print('Please enter ABO in the following format: A, B, AB or O') 
+            abo_input = str(input('Enter the ABO blood type entered: '))
+            if (abo_input.upper() == 'A') or (abo_input.upper() == 'B') or (abo_input.upper() == 'AB') or (abo_input.upper() == 'O'):
+                print(f'The blood type you entered was {abo_input.upper()}')
+                abo_inputted = True
+            else:
+                print("You entered an invalid input")
+        except ValueError:
+            print("You entered an invalid input")
+
+
+validate_abo_data()
+
+
+def validate_rh_data():
+    """
+    Validate Rh input data and add to sheet
+    """
+    rh_inputted = False
+    while rh_inputted is False:
+        try:
+            print('Please enter Rh status in the following format: POS or NEG') 
+            rh_input = str(input('Enter the Rh status entered: '))
+            if (rh_input.upper() == 'POS') or (rh_input.upper() == 'NEG'):
+                print(f'The blood type you entered was {rh_input.upper()}')
+                rh_inputted = True
+            else:
+                print("You entered an invalid input")
+        except ValueError:
+            print("You did not enter a string")
+
+
+validate_rh_data()

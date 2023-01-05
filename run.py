c1d5c579842284f23fdd4c272940c53780e782c5
BGCG <billie.griffith4@gmail.com>
2023-01-05 11:35:20 +0000

    Add validate stock input function to ensure user input is correct format

--- run.py
+++ run.py
@@ -19,31 +19,31 @@ stock = SHEET.worksheet('stock')
 donation_data = donations.get_all_values()
 stock_data = stock.get_all_values()
 
-options = ['APOS', 'ANEG', 'BPOS', 'BNEG', 'ABPOS', 'ABNEG', 'OPOS', 'ONEG']
+# options = ['APOS', 'ANEG', 'BPOS', 'BNEG', 'ABPOS', 'ABNEG', 'OPOS', 'ONEG']
 
 
-def validate_abo_data():
-    """
-    Validate blood type input data and add to sheet
-    """
-    abo_inputted = False
-    while abo_inputted is False:
-        try:
-            print('Please enter the ABO blood type followed by Rh status '
-                  'i.e. APOS, ONEG, ABPOS')
-            abo_input = str(input('Enter the ABO blood type: '))
-            if abo_input.upper() in options:
-                print(f'The blood type you entered was {abo_input.upper()}')
-                abo_inputted = True
-                donations.update_acell('A8', abo_input.upper())
-            else:
-                print("You entered an invalid input")
-            # return abo_input
-        except ValueError:
-            print("You entered an invalid input")
+# def validate_abo_data():
+#     """
+#     Validate blood type input data and add to sheet
+#     """
+#     abo_inputted = False
+#     while abo_inputted is False:
+#         try:
+#             print('Please enter the ABO blood type followed by Rh status '
+#                   'i.e. APOS, ONEG, ABPOS')
+#             abo_input = str(input('Enter the ABO blood type: '))
+#             if abo_input.upper() in options:
+#                 print(f'The blood type you entered was {abo_input.upper()}')
+#                 abo_inputted = True
+#                 donations.update_acell('A8', abo_input.upper())
+#             else:
+#                 print("You entered an invalid input")
+#             # return abo_input
+#         except ValueError:
+#             print("You entered an invalid input")
 
 
-validate_abo_data()
+# validate_abo_data()
 
 # DONATIONS_DATA = validate_abo_data()
 # print(DONATIONS_DATA.upper())
@@ -55,33 +55,39 @@ validate_abo_data()
 #     """
 #     donations.append_row(data, table_range="donations!A:A")
 
-def check_stock():
-    """
-    Check stock data
-    """
-    print('Calculating donations collected today...')
-    blood_count = donations.col_values(1)
-    apos_count = blood_count.count('APOS')
-    print('Number of A positive blood donations collected today:', apos_count)
-    aneg_count = blood_count.count('ANEG')
-    print('Number of A negative blood donations collected today:', aneg_count)
-    bpos_count = blood_count.count('BPOS')
-    print('Number of B positive blood donations collected today:', bpos_count)
-    bneg_count = blood_count.count('BNEG')
-    print('Number of B negative blood donations collected today:', bneg_count)
-    abpos_count = blood_count.count('ABPOS')
-    print('Number of AB positive blood donations collected today:',
-          abpos_count)
-    abneg_count = blood_count.count('ABNEG')
-    print('Number of AB negative blood donations collected today:', 
-          abneg_count)
-    opos_count = blood_count.count('OPOS')
-    print('Number of O positive blood donations collected today:', opos_count)
-    oneg_count = blood_count.count('ONEG')
-    print('Number of O negative blood donations collected today:', oneg_count)
+# def check_stock():
+#     """
+#     Check stock data
+#     """
+#     print('Calculating donations collected today...')
+#     blood_count = donations.col_values(1)
+#     apos_count = blood_count.count('APOS')
+#     print('Number of A positive blood donations collected today:'
+#           ', apos_count)
+#     aneg_count = blood_count.count('ANEG')
+#     print('Number of A negative blood donations collected today:'
+#           ', aneg_count)
+#     bpos_count = blood_count.count('BPOS')
+#     print('Number of B positive blood donations collected today:'
+#           ', bpos_count)
+#     bneg_count = blood_count.count('BNEG')
+#     print('Number of B negative blood donations collected today:'
+#           ', bneg_count)
+#     abpos_count = blood_count.count('ABPOS')
+#     print('Number of AB positive blood donations collected today:',
+#           abpos_count)
+#     abneg_count = blood_count.count('ABNEG')
+#     print('Number of AB negative blood donations collected today:',
+#           abneg_count)
+#     opos_count = blood_count.count('OPOS')
+#     print('Number of O positive blood donations collected today:'
+#           ', opos_count)
+#     oneg_count = blood_count.count('ONEG')
+#     print('Number of O negative blood donations collected today:'
+#           ', oneg_count)
 
 
-check_stock()
+# check_stock()
 
 
 def update_stock():
@@ -91,17 +97,30 @@ def update_stock():
     stock_inputted = False
     while stock_inputted is False:
         try:
-            print('Please input the blood stock today in eight digits in the '
-                  'following format 29, 25, 20, 50, 20, 29, 20, 19')
-            stock_input = []
-            stock_input = int(input('Please enter the stock for today: '))
-            length = len(stock_input)
-            print(length)
-            if length == 8:
-                print(f'You entered the following numbers - {stock_input}')
+            print('Please input the blood stock today in eight digits '
+                  'seperated by a space and no comma between')
+            stock_input = input('Please enter the number of donations'
+                                ' collected today: ').split()
+            stock_list = [int(item) for item in stock_input]
+            print(stock_list)
+            print(len(stock_list))
+            if len(stock_list) == 8:
+                print(f'You entered the following numbers - {stock_list}')
                 stock_inputted = True
+            else:
+                print('You enter the wrong number of numbers.' 
+                      'Please enter 8 sets')
         except ValueError:
-            print("You entered an invalid input")
+            print("You entered an invalid input - must be only numeric")
+            break
 
 
 update_stock()
+
+
+def update_stock_sheet():
+    """
+    Update stock data worksheet
+    """
+    pass
+
